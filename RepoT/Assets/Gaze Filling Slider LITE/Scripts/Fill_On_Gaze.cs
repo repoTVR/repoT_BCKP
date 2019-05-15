c64a9bd7dff674113e3ba5c4d9e9f3a60114edfe
davidebrunetto94 <37103188+davidebrunetto94@users.noreply.github.com>
2019-05-15 11:58:52 +0200

    Gaze filling slider

---
+++ RepoT/Assets/Gaze Filling Slider LITE/Scripts/Fill_On_Gaze.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Add this script to the object that has a Slider component
+/// attached to it.
+/// </summary>
+public class Fill_On_Gaze : MonoBehaviour
+{
+    //Public Variables.
+    [Header("How lond does it take to fill the slider:")]
+    public float fillTime; //How long it takes to fill the slider.
+
+    [Space][Space]
+    [Header("Do you want the slider to be disabled on start?")]
+    public bool disableOnStart;
+
+    [Space][Space]
+    [Header("Place the button we want to trigger on filling the slider:")]
+    public Button usedButton; //The button we want to use for function invoking when the slider is filled.
+
+    [Space]
+    [Space]
+    [Header("Animation Settings")]
+    public bool dontUseAnimation; //By default, we use an animation.
+    public enum AnimationOptions { Shake, ColorChange, Rescale } //Animation variations.
+    public AnimationOptions animationToPlay = AnimationOptions.ColorChange;
+
+    [Header("Place the animator playing the button animations.")]
+    public Animator animator;
+
+    //Private Variables.
+    private Slider mySlider; //The slider we work with.
+    private float timer; //We use this value in our coroutines.
+    private bool gazedAt; //Bool we use for checking if we are looking at something.
+    private Coroutine fillBarRoutine;
+
+    private void Start()
+    {
+        mySlider = GetComponent<Slider>();
+
+        //We check if we have a Slider component attached.
+        if (mySlider == null)
+        {
+            Debug.Log("No slider was found. Please attach this script only to objects that have a Slider component.");
+        }
+
+        //We check if we need to disable the slider on start.
+        if (disableOnStart)
+        {
+            mySlider.gameObject.SetActive(false);
+        }
+    }
+
+    //Method we invoke when our gaze is on the object.
+    public void PointerEnter()
+    {
+        if (disableOnStart)
+        {
+            mySlider.gameObject.SetActive(true);
+        }
+       
+        gazedAt = true;
+        usedButton.interactable = false; //We disable the button itself to prevent unwanted issues.
+        fillBarRoutine = StartCoroutine(FillBar());
+    }
+
+    //Method we invoke when our gaze is away from the object.
+    public void PointerExit()
+    {
+        if (disableOnStart)
+        {
+            mySlider.gameObject.SetActive(false);
+        }
+
+        gazedAt = false;
+
+        //Stop the routine if we have started it.
+        if (fillBarRoutine != null)
+        {
+            StopCoroutine(fillBarRoutine);
+        }
+
+        //Resetting values.
+        timer = 0f;
+        mySlider.value = 0f;
+    }
+
+    //Coroutine we use for changing the slider value.
+    private IEnumerator FillBar()
+    {
+        timer = 0f;
+
+        while (timer < fillTime)
+        {
+            timer += Time.deltaTime;
+            mySlider.value = timer / fillTime;
+            yield return null;
+
+            //If we are still looking at the object.
+            if (gazedAt)
+                continue;
+
+            timer = 0f;
+            mySlider.value = 0f;
+
+            yield break;
+        }
+
+        OnBarFilled();
+    }
+
+    //Method we use for finalizing the routine.
+    private void OnBarFilled()
+    {
+        mySlider.value = 0f;
+
+        //Depending on the animation settings, we play it.
+        if(animationToPlay == AnimationOptions.ColorChange)
+        {
+            animator.Play("ColorChange");
+        }
+        else if(animationToPlay == AnimationOptions.Shake)
+        {
+            animator.Play("Shake");
+        }
+        else if(animationToPlay == AnimationOptions.Rescale)
+        {
+            animator.Play("Rescale");
+        }
+
+        //We use the button as we are pressing it.
+        usedButton.onClick.Invoke();
+    }
+}
---
+++ RepoT/Assets/Gaze Filling Slider LITE/Scripts/Gaze_Interaction_Input_Module.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine.UI;
+
+/// <summary>
+/// Add this script to the EventSystem object in your scene.
+/// This script creates a custom input module based on the one provided
+/// by the OVR package.
+/// </summary>
+namespace UnityEngine.EventSystems
+{
+    public class Gaze_Interaction_Input_Module : OVRInputModule
+    {
+        //Public Variables.
+        [Tooltip("How long to wait until a gazed control is activated")]
+        [Header("How long you will wait until a gazed control is activated?")]
+        public float activationDwellTime = 1.0f;
+
+        //Private Variables.
+        private GameObject currentlyGazedObject;
+        private float lastTimeGazedObjectChanged;
+
+        override public void Process()
+        {
+            base.Process();
+
+            //Handle selection.
+            if (!currentlyGazedObject || lastTimeGazedObjectChanged > Time.time)
+            {
+                OVRGazePointer.instance.SelectionProgress = 0f;
+            }
+            else
+            {
+                float currentDwellTime = Time.time - lastTimeGazedObjectChanged;
+                OVRGazePointer.instance.SelectionProgress = currentDwellTime / activationDwellTime;
+            }
+        }
+
+        /// <summary>
+        /// Overwritten from the base version so we can use our custom. 
+        /// </summary>
+        /// <returns></returns>
+        override protected MouseState GetGazePointerData()
+        {
+            MouseState state = base.GetGazePointerData();
+
+            var raycast = state.GetButtonState(PointerEventData.InputButton.Left).eventData.buttonData.pointerCurrentRaycast;
+
+            //Get custom press state.
+            PointerEventData.FramePressState pressState = GetGazeButtonState(raycast.gameObject);
+            //Set custom press state.
+            state.SetButtonState(PointerEventData.InputButton.Left, pressState, state.GetButtonState(PointerEventData.InputButton.Left).eventData.buttonData);
+
+            return state;
+        }
+
+        /// <summary>
+        /// Modified version of the base class that takes into account when which object got hit.
+        /// </summary>
+        /// <returns></returns>
+        protected PointerEventData.FramePressState GetGazeButtonState(GameObject rayCastHit)
+        {
+            var pressed = Input.GetKeyDown(gazeClickKey) || OVRInput.GetDown(joyPadClickButton);
+            var released = Input.GetKeyUp(gazeClickKey) || OVRInput.GetUp(joyPadClickButton);
+
+            bool shouldImmediatelyRelease;
+            GameObject newGazedObject = GetCurrentlyGazedGameObject(rayCastHit, out shouldImmediatelyRelease);
+            if (currentlyGazedObject != newGazedObject)
+            {
+                released |= true;
+                currentlyGazedObject = newGazedObject;
+                lastTimeGazedObjectChanged = Time.time;
+            }
+
+            float currentDwellTime = Time.time - lastTimeGazedObjectChanged;
+            if (currentlyGazedObject && currentDwellTime >= activationDwellTime)
+            {
+                pressed |= true;
+                if (shouldImmediatelyRelease)
+                {
+                    //Reset the time so this doesn't get activated again.
+                    lastTimeGazedObjectChanged = float.MaxValue;
+                    released |= true; //Simulate click.
+                }
+            }
+
+            if (pressed && released)
+                return PointerEventData.FramePressState.PressedAndReleased;
+            if (pressed)
+                return PointerEventData.FramePressState.Pressed;
+            if (released)
+                return PointerEventData.FramePressState.Released;
+            return PointerEventData.FramePressState.NotChanged;
+        }
+
+        //Method for receiving information about the currently gazed object.
+        private GameObject GetCurrentlyGazedGameObject(GameObject go, out bool shouldImmediatelyRelease)
+        {
+            shouldImmediatelyRelease = true;
+            if (!go)
+            {
+                return null;
+            }
+
+            Slider slider = go.GetComponentInParent<Slider>();
+            if (slider)
+            {
+                shouldImmediatelyRelease = false;
+                return slider.gameObject;
+            }
+            Button button = go.GetComponentInParent<Button>();
+            if (button)
+            {
+                return button.gameObject;
+            }
+            Toggle toggle = go.GetComponentInParent<Toggle>();
+            if (toggle)
+            {
+                return toggle.gameObject;
+            }
+
+            return null;
+        }
+
+        public void SetActivationDwellTime(float v)
+        {
+            activationDwellTime = v;
+        }
+    }
+}
